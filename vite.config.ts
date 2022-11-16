5465738116cfb6594c4ccefc1eb51d4855cab456
Jean-François Larosa <44083166+jflarosa@users.noreply.github.com>
2022-11-16 09:04:07 +0100

    chore: create folders and update paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [{ find: "@utils", replacement: path.resolve(__dirname, "src/utils/index") }]
+    alias: [
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils/index') },
+      {
+        find: '@ui',
+        replacement: path.resolve(__dirname, 'src/ui/index'),
+      },
+      { find: '@features', replacement: path.resolve(__dirname, 'src/features/index') },
+      {
+        find: '@data',
+        replacement: path.resolve(__dirname, 'src/data/index'),
+      },
+      {
+        find: '@router',
+        replacement: path.resolve(__dirname, 'src/router/index'),
+      },
+    ],
   },
-  plugins: [react()]
+  plugins: [react()],
 });
